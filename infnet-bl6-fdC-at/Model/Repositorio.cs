820148ab3160aea437be5f8abd576c06c272ad46
Juarez Geraldo Silva Jr <juarezgeraldosilva@gmail.com>
2022-12-08 13:15:01 -0300

    Adicionar arquivos de projeto.

---
+++ infnet-bl6-fdC-at/Model/Repositorio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Funcoes;
+using PessoaAniversario;
+
+namespace _Repositorio
+{
+    public class Repositorio
+    {
+        private List<Pessoa> _listaPessoa = new List<Pessoa>();
+        private int _pessoaId;
+
+        public void AdicionarPessoa(Pessoa pessoa)
+        {
+            pessoa.pessoaId = _pessoaId;
+            
+            _listaPessoa.Add(pessoa);
+            _pessoaId += 1;
+        }
+        public List<Pessoa> ListaPessoas()
+        {
+            return _listaPessoa;
+        }
+        public Pessoa PesquisaPessoa(Pessoa pessoa)
+        {
+            return _listaPessoa[_listaPessoa.IndexOf(pessoa)];
+        }
+        public void ImprimirPessoa(Pessoa pessoa)
+        {
+            Console.WriteLine(pessoa.pessoaId + " - " + pessoa.nomeCompleto);
+        }
+    }
+}
---
+++ infnet-bl6-fdC-at/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Collections;
+using Funcoes;
+
+namespace AplicacaoPrincipal
+
+{
+    public class Programa
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+            Funcoes.Funcoes funcao = new Funcoes.Funcoes();
+
+            string arquivo = funcao.verificaArquivo();
+            Console.WriteLine(arquivo);
+
+            while (opcao != 9)
+            {
+                Console.WriteLine("Gerenciador de aniversários");
+                Console.WriteLine("===========================");
+                Console.WriteLine();
+                Console.WriteLine("Selecione uma das opções abaixo:");
+                Console.WriteLine("1 - Adicionar pessoa");
+                Console.WriteLine("2 - Alterar pessoas");
+                Console.WriteLine("3 - Excluir pessoas");
+                Console.WriteLine("4 - Pesquisar pessoas");
+                Console.WriteLine("5 - Pesquisar aniversário");
+                Console.WriteLine();
+                Console.WriteLine("9 - Sair");
+
+                while (!int.TryParse(Console.ReadLine(), out opcao))
+                {
+                    Console.WriteLine("Valor informado está inválido");
+                };
+
+                Console.Clear();
+
+                switch (opcao)
+                {
+                    case 1:
+                        funcao.cadastraPessoa();
+                        break;
+                    case 4:
+                        funcao.pesquisaPessoa();
+                        break;
+                    default:
+                        break;
+                }
+                Console.Clear();
+            }
+
+        }
+    }
+}
