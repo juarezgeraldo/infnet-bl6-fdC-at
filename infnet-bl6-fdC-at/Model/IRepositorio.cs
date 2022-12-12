5bf3d109f5b07234d5062292a4ebaf964f8da1e5
Juarez Geraldo Silva Jr <juarezgeraldosilva@gmail.com>
2022-12-12 19:44:28 -0300

    Atualização Git

---
+++ infnet-bl6-fdC-at/Model/IRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using PessoaAniversario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _IRepositorio
+{
+    internal interface IRepositorio
+    {
+        public List<Pessoa> CarregaPessoasLista(string arquivo);
+        public void AdicionarPessoa(Pessoa pessoa, string arquivo);
+        public void AlterarPessoa(string arquivo);
+        public void ExcluirPessoa(string arquivo);
+        public Dictionary<int, int> PesquisaPessoas(string nomePesq);
+        public List<Pessoa> PesquisaAniversarioHoje();
+    }
+}
